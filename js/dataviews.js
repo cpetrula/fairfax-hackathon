b06b6a222ff16e352b050082b1f8e92db3f5eaf5
cpetrula <cpetrula@yahoo.com>
2018-05-09 15:34:40 -0400

    add voice code

---
+++ js/dataviews.js
@@ -0,0 +1,336 @@
+
+//  Datasheet should be pulled from /data/all_data.json
+var datasheet = {
+    "CoverageRate_CGL.xlsx":
+        {
+            "Sheet1":
+                [
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Residential Plumber",
+                        "ibcCode": 171101,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 109,
+                        "revenueRange-UP_TO_100K": 157,
+                        "revenueRange-UP_TO_250K": 362,
+                        "revenueRange-UP_TO_500K": 760,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Residential Painter",
+                        "ibcCode": 175401,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 55,
+                        "revenueRange-UP_TO_500K": 116,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Tree Removal/Pruning",
+                        "ibcCode": 73601,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 77,
+                        "revenueRange-UP_TO_100K": 115,
+                        "revenueRange-UP_TO_250K": 265,
+                        "revenueRange-UP_TO_500K": 557,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Landscaper",
+                        "ibcCode": 73501,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 46,
+                        "revenueRange-UP_TO_250K": 105,
+                        "revenueRange-UP_TO_500K": 221,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Residential Electrician",
+                        "ibcCode": 173101,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 48,
+                        "revenueRange-UP_TO_500K": 100,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Drywaller",
+                        "ibcCode": 174401,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 50,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Framing Contractor",
+                        "ibcCode": 181102,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 80,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Cleaner (mostly residential)",
+                        "ibcCode": 739401,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 82,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Finish Carpenter",
+                        "ibcCode": 175201,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 64,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Fences & Decks Contractor",
+                        "ibcCode": 152201,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 79,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Handyman/Handywoman",
+                        "ibcCode": 175202,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 70,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "HVAC Contractor (Residential Only)",
+                        "ibcCode": 171501,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 57,
+                        "revenueRange-UP_TO_100K": 79,
+                        "revenueRange-UP_TO_250K": 183,
+                        "revenueRange-UP_TO_500K": 384,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Flooring Installer",
+                        "ibcCode": 175601,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 53,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Masonry Contractor",
+                        "ibcCode": 174301,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 54,
+                        "revenueRange-UP_TO_500K": 114,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Window/Door/Siding Contractor",
+                        "ibcCode": 176601,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 86,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Tiling Contractor",
+                        "ibcCode": 174501,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 52,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Paving Contractor (Residential)",
+                        "ibcCode": 152101,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 61,
+                        "revenueRange-UP_TO_100K": 86,
+                        "revenueRange-UP_TO_250K": 199,
+                        "revenueRange-UP_TO_500K": 417,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Locksmith",
+                        "ibcCode": 769301,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 42,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Paving Contractor (Commercial)",
+                        "ibcCode": 152102,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 61,
+                        "revenueRange-UP_TO_100K": 86,
+                        "revenueRange-UP_TO_250K": 199,
+                        "revenueRange-UP_TO_500K": 417,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Commercial Plumber",
+                        "ibcCode": 171103,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 109,
+                        "revenueRange-UP_TO_100K": 157,
+                        "revenueRange-UP_TO_250K": 362,
+                        "revenueRange-UP_TO_500K": 760,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "HVAC Contractor (Commercial)",
+                        "ibcCode": 171502,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 58,
+                        "revenueRange-UP_TO_100K": 81,
+                        "revenueRange-UP_TO_250K": 187,
+                        "revenueRange-UP_TO_500K": 392,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Window & Eaves Cleaning",
+                        "ibcCode": 739402,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 74,
+                        "revenueRange-UP_TO_500K": 156,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Commercial Electrician",
+                        "ibcCode": 173102,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 49,
+                        "revenueRange-UP_TO_500K": 103,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Furniture & Rug Cleaning",
+                        "ibcCode": 739501,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 42,
+                        "revenueRange-UP_TO_500K": 86,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Residential Internet/Phone/Cable Installation",
+                        "ibcCode": 153401,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 42,
+                        "revenueRange-UP_TO_250K": 67,
+                        "revenueRange-UP_TO_500K": 141,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Air Duct Cleaning (Residential)",
+                        "ibcCode": 171505,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 57,
+                        "revenueRange-UP_TO_100K": 79,
+                        "revenueRange-UP_TO_250K": 183,
+                        "revenueRange-UP_TO_500K": 384,
+                        "coverageLimit": "UP_TO_2M"
+                    },
+                    {
+                        "coverage_name_web": "Commercial General Liability",
+                        "primaryName": "Solar Energy Contractors",
+                        "ibcCode": 171901,
+                        "province_code": "ON",
+                        "revenueRange-UP_TO_50K": 42,
+                        "revenueRange-UP_TO_100K": 50,
+                        "revenueRange-UP_TO_250K": 116,
+                        "revenueRange-UP_TO_500K": 244,
+                        "coverageLimit": "UP_TO_2M"
+                    }
+                ]
+        }
+}
+
+var commercial_general_liability = (datasheet["CoverageRate_CGL.xlsx"] && datasheet["CoverageRate_CGL.xlsx"]["Sheet1"]) || []
+
+
+var getInitialQuotes =  function(primaryName, province, revenue) {
+
+    if (revenue <= 50000) {
+        revenue_lookup = "revenueRange-UP_TO_50K";
+    } else if (revenue <= 100000) {
+        revenue_lookup = "revenueRange-UP_TO_100K";
+    } else if (revenue <= 250000) {
+        revenue_lookup = "revenueRange-UP_TO_250K";
+    } else if (revenue <= 500000) {
+        revenue_lookup = "revenueRange-UP_TO_250K";
+    }
+
+    var results = [];
+    var mainList = (comemercial_general_liability["CoverageRate_CGL.xlsx"] && gpl_data["CoverageRate_CGL.xlsx"]["Sheet1"]) || []
+    for (i = 0; i < mainList.length; i++) {
+        var pn = mainList[i].primaryName;
+        var pv = mainList[i].province_code;
+
+        if (primaryName == pn && province == pv) {
+            result = {quote: mainList[revenue_lookup], coverageLimit: mainList.coverageLimit};
+            results.push(result);
+        }
+    }
+    return results;
+}
